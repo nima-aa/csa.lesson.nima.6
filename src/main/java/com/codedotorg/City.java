6c04ca5d939e27ed122661aba4b3879f6c50d059
Nima Abdirahman <Nima_Abdirahman26@rdale.org>
2024-04-22 12:39:18 -0500

    test

--- src/main/java/com/codedotorg/City.java
+++ src/main/java/com/codedotorg/City.java
@@ -1,6 +1,6 @@
 package com.codedotorg;
 
-public class City {
+public class City implements Comparable<City> {
     
     /** The name of a City */
     private String name;
@@ -22,4 +22,29 @@ public class City {
         return name;
     }
 
+}
+
+@Override
+public int compareTo(City otherCity) {
+    // Compare city names lexicographically
+    return this.name.compareTo(otherCity.name);
+}
+import java.util.Arrays;
+
+public class CitySorter {
+    public static void main(String[] args) {
+        City[] cities = {
+            new City("New York"),
+            new City("Los Angeles"),
+            new City("Chicago"),
+            new City("Seattle")
+        };
+
+        Arrays.sort(cities);
+
+        System.out.println("Cities in alphabetical order:");
+        for (City city : cities) {
+            System.out.println(city.getName());
+        }
+    }
 }
